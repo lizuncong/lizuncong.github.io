6e5684c9e174987c08c8d07fa8102d7d792c59b5
lizuncong <2900170896@qq.com>
2023-01-21 16:47:11 +0800

    feat: 添加菜单

--- src/menu.jsx
+++ src/menu.jsx
@@ -1,4 +1,32 @@
 import dynamicMenus from "@/dynamic/menus";
-const MENU = [...dynamicMenus];
+const MENU = [
+  {
+    label: (
+      <a
+        className="custom-menu"
+        target="_blank"
+        href="https://lizuncong.github.io/excalidraw-app/"
+        rel="noreferrer"
+      >
+        Canvas教程
+      </a>
+    ),
+    key: "https://lizuncong.github.io/excalidraw-app/",
+  },
+  {
+    label: (
+      <a
+        className="custom-menu"
+        target="_blank"
+        href="https://lizuncong.github.io/react-ui/"
+        rel="noreferrer"
+      >
+        React组件
+      </a>
+    ),
+    key: "https://lizuncong.github.io/react-ui/",
+  },
+  ...dynamicMenus,
+];
 
 export default MENU;
