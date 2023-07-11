49e0ee17c753c39fd606b3bed0692a26025f4971
lizuncong <2900170896@qq.com>
2023-07-11 23:43:03 +0800

    feat: stash

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -748,7 +748,7 @@ module.exports = function (webpackEnv) {
           extensions: ["js", "mjs", "jsx", "ts", "tsx"],
           formatter: require.resolve("react-dev-utils/eslintFormatter"),
           eslintPath: require.resolve("eslint"),
-          failOnError: !(isEnvDevelopment && emitErrorsAsWarnings),
+          failOnError: false,
           context: paths.appSrc,
           cache: true,
           cacheLocation: path.resolve(
