f8735f69afd4a91d60e3f32559f26775d5378b30
lizuncong <2900170896@qq.com>
2023-09-06 23:08:07 +0800

    feat: 事件循环

--- test.js
+++ test.js
@@ -1,26 +1,24 @@
-const http = require('http')
+const http = require('http');
+const rp = require('request-promise');
 
 const server = http.createServer((req, res) => {
-    res.write(`${startCount() + nextCount()}`)
-    res.end();
+    Promise.all([startCount(), nextCount()]).then((values) => {
+        const sum = values.reduce((prev, curr, idx, arr) => {
+            return parseInt(prev) + parseInt(curr)
+        })
+        res.write(`${sum}`);
+        res.end();
+    })
 })
 
 server.listen(4000, () => {
     console.log('server start http://127.0.0.1:4000')
 })
 
-function startCount(){
-    let sum = 0;
-    for(let i = 0; i < 500000000;i++){
-        sum = sum + i
-    }
-    return sum
+async function startCount() {
+    return await rp.get('http://127.0.0.1:5000')
 }
 
-function nextCount(){
-    let sum = 0;
-    for(let i = 500000000; i < 1000000000;i++){
-        sum = sum + i
-    }
-    return sum
+async function nextCount() {
+    return await rp.get('http://127.0.0.1:6000')
 }
