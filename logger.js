b5820ae885e2b8994f20df5b113cf7ba91638287
lizuncong <2900170896@qq.com>
2023-09-11 22:40:17 +0800

    feat: 缓存

--- logger.js
+++ logger.js
@@ -1,84 +1,75 @@
-const platform = process.platform;
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
-const os = require('os');
 
-// info会调用_flush判断当前缓存是否已满，或是否开启了缓存。如没有开启或缓存已满，则直接调用
-//_addLog写入日志。其他情况都写入缓存中。
+const { promisify } = require('util');
+const redis = require('redis');
+const NodeCache = require('node-cache');
 
-// start会定时从临时缓存中获取待写入日志内容，如有则调用_addLog写入日志。写入完后
-// 会调用_clean清理已写入内容，其次会清理未使用的空文件流句柄，避免空文件夹、句柄流
-// 占用缓存
-
-// _addLog根据类型获取需要写入的文件路径，并调用_getFileStream来获取文件流句柄
-// 这里就会用到文件流缓存，当缓存有则返回，没有则创建一个文件流句柄
-class Logger {
-
-    /**
-     * 
-     * @param {boolean} cacheEnable 是否打开日志缓存模式，默认打开 
-     * @param {int} cacheTime 缓存处理时间，默认2秒，会定时写入文件 
-     * @param {int} maxLen 单个日志文件最大缓存长度，默认100000 
-     * @param {int} maxFileStream 最大缓存文件句柄数，默认是1000
-     */
-    constructor(cacheEnable = true, cacheTime = 2000, maxLen = 100000, maxFileStream = 1000) {
-        this.cacheEnable = cacheEnable;
-        this.cacheTime = cacheTime;
-        this.maxLen = maxLen;
-        this.maxFileStream = maxFileStream;
-        this.currentFileStreamNum = 0;
+class Cache {
+  constructor(localCacheEnable = true, redisEnable = true){
+    this.localCacheEnable = localCacheEnable;
+    this.redisEnable = redisEnable;
+    
+    if(localCacheEnable){
+      this.localCache = new NodeCache();
     }
-    _flush(fileType, logInfo) {
-        if (!fileType) {
-            return
-        }
-        let logStr = logInfo;
-        if (typeof logInfo === 'object') {
-            logStr = JSON.stringify(logInfo)
-        }
-        if (logStr === '' || !logStr) {
-            return;
-        }
-        if (!this.cacheEnable) {
-            // 缓存关闭，直接写日志
-            return this._addLog(fileType, cacheLogStr[fileType])
-        }
-        if (!cacheLogStr[fileType]) {
-            // 判断是否已经有缓存
-            return cacheLogStr[fileType] = `${logStr}`
-        }
-        if (cacheLogStr[fileType].length < this.maxLen) {
-            // 判断是否已经超出缓存最大尺度
-            return cacheLogStr[fileType] = `${cacheLogStr[fileType]}]\n${logStr}`
-        } else {
-            // 如果超出则直接写入日志
-            return this._addLog(fileType, cacheLogStr[fileType])
-        }
+
+    if(redisEnable){
+      this.redisClient = redis.createClient({
+        host: 'redis-18568.c12.us-east-1-4.ec2.cloud.redislabs.com',
+        port: 18568,
+        password: 'eI1cKZZ87CSHcr9eeRO142ORHJtJCYfO',
+        db: 0,
+      });
     }
-    _intervalWrite() {
-        setInterval(() => {
-            if (Object.keys(cacheLogStr).length < 1) {
-                // 空数据不处理
-                return;
-            }
-            for (let fileType in cacheLogStr) {
-                // 遍历需要写入的日志信息
-                if (cacheLogStr[fileType] === '') {
-                    //空数据，需要清理句柄。对空缓存的文件流句柄进行清理，
-                    // 避免一些没用句柄缓存一直占用缓存数据
-                    this._clean(fileType).then();
-                    continue;
-                }
-                // 写入日志，写入完成后，需要清理当前的日志缓存，注意这里可能会导致
-                // 日志丢失。主要在写入日志完成后，还没清理，又有数据写入临时缓存中，导致
-                // 部分丢失的现象，默认情况下允许这种情况，因为这种丢失的现象
-                // 概率比较低
-                this._addLog(fileType, cacheLogStr[fileType]).then(() => {
-                    cacheLogStr[fileType] = ''
-                })
-            }
-        }, this.cacheTime);
+  }
+
+  /**
+   * 获取缓存数据
+   * @param {string} key 
+   * @returns 
+   */
+  async get(key){
+    let value;
+    if(this.localCacheEnable){
+      value = this.localCache.get(key);
+      console.log(`local cache value is ${value}`);
     }
+    if(!value && this.redisEnable){
+      try {
+        value = await promisify(this.redisClient.get).bind(this.redisClient)(key);
+        console.log(`redis cache value is ${value}`)
+      } catch(error){
+        console.log(error);
+      }
+    }
+    return value;
+  }
+
+  /**
+   * 设置保存值
+   * @param {string} key 缓存key 
+   * @param {string} value 缓存值
+   * @param {number} exprie 过期时间/秒
+   * @param {*} cacheLocal 是否本地缓存
+   * @returns 
+   */
+  async set(key, value, exprie = 10, cacheLocal = false){
+    let localCacheRet, redisRet;
+    if(this.localCacheEnable){
+      localCacheRet = this.localCache.set(key, value, exprie);
+    }
+
+    if(this.redisEnable){
+      try {
+        redisRet = await promisify(this.redisClient.set).bind(this.redisClient)(key, value, 'EX', exprie)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    return localCacheRet || redisRet;
+  }
+
 }
 
-module.exports = Logger;
+module.exports = (localCacheEnable = true, redisEnable = true) => {
+  return new Cache(localCacheEnable, redisEnable)
+}
