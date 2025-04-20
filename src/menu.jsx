3e774647ec9790d20b665672d3058be62dc16979
lizuncong <2900170896@qq.com>
2025-04-20 11:13:03 +0800

    feat: 菜单整理

--- src/menu.jsx
+++ src/menu.jsx
@@ -52,7 +52,123 @@ const MENU = [
     ),
     key: "https://lizuncong.github.io/react-ui/",
   },
-  ...dynamicMenus,
+  {
+    label: (
+      <a
+        className="custom-menu"
+        target="_blank"
+        href="https://github.com/lizuncong/egg-react-ssr"
+        rel="noreferrer"
+      >
+        React服务端渲染实战
+      </a>
+    ),
+    key: "https://github.com/lizuncong/egg-react-ssr",
+  },
+  {
+    label: (
+      <a
+        className="custom-menu"
+        target="_blank"
+        href="https://github.com/lizuncong/mini-react"
+        rel="noreferrer"
+      >
+        React源码深度解析
+      </a>
+    ),
+    key: "https://github.com/lizuncong/mini-react",
+  },
+  {
+    label: (
+      <a
+        className="custom-menu"
+        target="_blank"
+        href="https://github.com/lizuncong/mini-webpack"
+        rel="noreferrer"
+      >
+        手写Webpack源码
+      </a>
+    ),
+    key: "https://github.com/lizuncong/mini-webpack",
+  },
+  {
+    label: (
+      <a
+        className="custom-menu"
+        target="_blank"
+        href="https://github.com/lizuncong/mini-webpack-dev-server"
+        rel="noreferrer"
+      >
+        手写Webpack Dev Server源码
+      </a>
+    ),
+    key: "https://github.com/lizuncong/mini-webpack-dev-server",
+  },
+  {
+    label: (
+      <a
+        className="custom-menu"
+        target="_blank"
+        href="https://github.com/lizuncong/mini-tapable"
+        rel="noreferrer"
+      >
+        手写WebpackTapable源码
+      </a>
+    ),
+    key: "https://github.com/lizuncong/mini-tapable",
+  },
+  {
+    label: (
+      <a
+        className="custom-menu"
+        target="_blank"
+        href="https://github.com/lizuncong/mini-react-redux"
+        rel="noreferrer"
+      >
+        手写React Redux源码
+      </a>
+    ),
+    key: "https://github.com/lizuncong/mini-react-redux",
+  },
+  {
+    label: (
+      <a
+        className="custom-menu"
+        target="_blank"
+        href="https://github.com/lizuncong/mini-react-router"
+        rel="noreferrer"
+      >
+        手写React Router源码
+      </a>
+    ),
+    key: "https://github.com/lizuncong/mini-react-router",
+  },
+  {
+    label: (
+      <a
+        className="custom-menu"
+        target="_blank"
+        href="https://github.com/lizuncong/mini-promise"
+        rel="noreferrer"
+      >
+        手写Promise源码
+      </a>
+    ),
+    key: "https://github.com/lizuncong/mini-promise",
+  },
+  {
+    label: (
+      <a
+        className="custom-menu"
+        target="_blank"
+        href="https://github.com/lizuncong/mini-koa"
+        rel="noreferrer"
+      >
+        手写Koa源码
+      </a>
+    ),
+    key: "https://github.com/lizuncong/mini-koa",
+  },
 ];
 
 export default MENU;
