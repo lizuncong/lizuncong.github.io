44f808c49f874ca307d3baf43cd3bf31b36caa88
lizuncong <2900170896@qq.com>
2025-08-13 20:16:41 +0800

    Update index.jsx

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,13 +2,10 @@ import React from "react";
 
 function Index() {
   return <div className="home" onClick={() => {
-    gtag('event', 'screen_view', {
-        'app_name': 'myAppName',
-        'screen_name': 'Home'
-      });
         gtag('event', 'hello_test', {
         'app_name': 'myAppName',
-        'screen_name': 'Home'
+        'screen_name': 'Home',
+        '_my_test_time_': new Date().toLocaleString()
       });
 }}>首页</div>;
 }
