8ca0168dbb167a2b528db8691a2318182f21dd98
lizuncong <2900170896@qq.com>
2025-12-09 00:07:33 +0800

    fix: home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Markdown from "@/components/markdown";
 function Index() {
   return <div className="home" onClick={() => {
         gtag('event', 'hello_test', {
@@ -7,7 +7,9 @@ function Index() {
         'screen_name': 'Home',
         'my_test_time': new Date().toLocaleString()
       });
-}}>首页</div>;
+}}>
+  <Markdown src={"/#/profile"} />
+</div>;
 }
 
 export default Index;
